265649601f6434914c0bc96fdac0f227df15c5ce
Magikarp4000 <90388135+Magikarp4000@users.noreply.github.com>
2024-08-05 17:51:36 +1200

    Add random_argmax to agents.py
    - Change on-policy to be incremental step-wise instead of episode-wise
    - Attempt epsilon-soft off-policy

--- agents.py
+++ agents.py
@@ -41,12 +41,12 @@ class Agent(ABC):
                 self._metadata = to_load[name]
             elif name in self._data:
                 setattr(self, name, to_load[name])
-        self._load_convert()
+        self.load_convert()
         self.core_init()
         print(f'Loaded {file_name}.json!')
     
     def save(self, file_name=None):
-        self._save_convert()
+        self.save_convert()
         to_save = {'metadata': self._metadata}
         for val in self._data:
             try:
@@ -55,10 +55,10 @@ class Agent(ABC):
                 print(f"SAVE_ERROR: Attribute '{val}' doesn't exist!")
         data_transfer.save(file_name, to_save)
 
-    def _load_convert(self):
+    def load_convert(self):
         pass
 
-    def _save_convert(self):
+    def save_convert(self):
         pass
 
     @abstractmethod
@@ -78,8 +78,6 @@ class DPAgent(Agent):
     def train(self, algo, gamma=0.9, theta=1, record=False, save_params=True, save_time=True):
         if save_params:
             self.config_meta({'algo': algo, 'gamma': gamma, 'theta': theta})
-        if save_time:
-            self.config_meta({'time': str(datetime.datetime.now())})
         self.size = len(self.states)
         self.v = np.array([0 for _ in range(self.size + 1)], dtype='float')
         self.pi = np.array([0 for _ in range(self.size + 1)], dtype='int')
@@ -91,6 +89,8 @@ class DPAgent(Agent):
             self.value_iteration(gamma, theta, record)
         else:
             print("Invalid algorithm mate")
+        if save_time:
+            self.config_meta({'time': str(datetime.datetime.now())})
 
     def policy_iteration(self, gamma, theta, record=False):
         num = 1
@@ -167,11 +167,11 @@ class DPAgent(Agent):
                 self.v_record.append((s, self.v[s]))
         return delta
     
-    def _load_convert(self):
+    def load_convert(self):
         self.v = np.array(self.v)
         self.pi = np.array(self.pi)
     
-    def _save_convert(self):
+    def save_convert(self):
         self.v = self.v.tolist()
         self.pi = self.pi.tolist()
     
@@ -188,83 +188,105 @@ class MCAgent(Agent):
         super().__init__()
         self.config(['q', 'pi', 'q_record', 'pi_record'])
         self.q = [[]]
-        self.pi = np.array([])
+        self.pi = []
         self.q_record = []
         self.pi_record = []
         self.b = []
     
-    def train(self, algo, num_epsd, gamma=1, eps=0.1, save_params=True, save_time=True):
+    def train(self, algo, num_epsd, gamma=1, eps=0.1, q=None, pi=None, batch_size=1, save_params=True, save_time=True):
         if save_params:
             self.config_meta({'algo': algo, 'num_epsd': num_epsd, 'gamma': gamma, 'eps': eps})
-        if save_time:
-            self.config_meta({'time': datetime.datetime.now()})
-        self.q = [[0 for _ in self.actions[s]] for s in range(self.size)]
-        self.pi = np.array([0 for _ in self.states], dtype='int')
+        if q:
+            self.q = q
+        else:
+            self.q = [[0 for _ in self.actions[s]] for s in range(self.size)]
+        if pi:
+            self.pi = pi
+        else:
+            self.pi = [random_argmax(self.q[s]) for s in range(self.size)]
         self.q_record.clear()
         self.pi_record.clear()
         if algo == 'onpolicy':
-            self.onpolicy(num_epsd, gamma, eps)
+            self.onpolicy(num_epsd, gamma, eps, batch_size)
         elif algo == 'offpolicy':
-            self.offpolicy(num_epsd, gamma)
+            self.offpolicy(num_epsd, gamma, batch_size)
         else:
             print("Invalid algorithm mate")
+        if save_time:
+            self.config_meta({'time': str(datetime.datetime.now())})
     
     def get_action(self, s):
         return np.random.choice(len(self.actions[s]), p=self.b[s])
     
-    def on_epsd(self, gamma, eps, seq, returns, nums):
+    def on_epsd(self, gamma, eps, seq, nums):
+        visited = set()
         g = 0
         for step in reversed(seq):
             s, a, reward = step
             g = reward + gamma * g
-            returns[s][a].append(g)
-        for s in range(self.size):
-            for a in range(len(self.actions[s])):
-                if returns[s][a]:
-                    x = len(returns[s][a])
-                    self.q[s][a] += (sum(returns[s][a]) - x * self.q[s][a]) / (nums[s][a] + x)
-                    nums[s][a] += x
-                    returns[s][a].clear()
+            temp = nums[s][a] if nums[s][a] else 1
+            self.q[s][a] += 1 / temp * (g - self.q[s][a])
+            nums[s][a] += 1
+            visited.add(s)
+        for s in visited:
             old_pi = self.pi[s]
-            self.pi[s] = np.argmax(self.q[s])
+            self.pi[s] = random_argmax(self.q[s])
+            # self.b[s] = np.full(len(self.actions[s]), eps / len(self.actions[s]))
+            # self.b[s][self.pi[s]] += 1 - eps
             self.b[s][old_pi] = eps / len(self.actions[s])
             self.b[s][self.pi[s]] = 1 - eps + eps / len(self.actions[s])
-        return returns, nums
+        return nums
 
-    def onpolicy(self, num_epsd, gamma, eps, batch_size=10000):
+    def onpolicy(self, num_epsd, gamma, eps, batch_size):
         self.b = [np.full(len(self.actions[s]), eps / len(self.actions[s]))
                   for s in range(self.size)]
         for s in range(self.size):
             self.b[s][self.pi[s]] += 1 - eps
         nums = [np.zeros(len(self.actions[s])) for s in range(self.size)]
-        returns = [[[] for _ in self.actions[s]] for s in range(self.size)]
         num = 0
+        real_num = 0
         for num in range(num_epsd // batch_size):
             start_time = time.time()
             for _ in range(batch_size):
+                real_num += 1
                 seq = self.get_episode()
-                returns, nums = self.on_epsd(gamma, eps, seq, returns, nums)
+                # eps = max(0.1, min(1, 1 / np.sqrt(real_num)))
+                nums = self.on_epsd(gamma, eps, seq, nums)
             end_time = time.time()
             print(f"Episodes {num * batch_size} - {(num + 1) * batch_size} complete in {round(end_time - start_time, 2)}s.")
             num += 1
 
     def off_epsd(self, gamma, seq, c):
+        eps = 0.1
+        visited = set()
         g = 0
         ratio = 1
         for step in reversed(seq):
             s, a, reward = step
+            visited.add(s)
             g = reward + gamma * g
             c[s][a] += ratio
             self.q[s][a] += ratio / c[s][a] * (g - self.q[s][a])
             self.pi[s] = np.argmax(self.q[s])
             if a != self.pi[s]:
+                for t in visited:
+                    self.b[t] = np.full(len(self.actions[t]), eps / len(self.actions[t]))
+                    self.b[t][self.pi[t]] += 1 - eps
                 return c
             ratio *= 1 / self.b[s][a]
+        for s in visited:
+            self.b[s] = np.full(len(self.actions[s]), eps / len(self.actions[s]))
+            self.b[s][self.pi[s]] += 1 - eps
         return c
 
-    def offpolicy(self, num_epsd, gamma, batch_size=10000):
-        self.b = [np.full(len(self.actions[s]), 1 / len(self.actions[s]))
+    def offpolicy(self, num_epsd, gamma, batch_size):
+        eps = 0.1
+        self.b = [np.full(len(self.actions[s]), eps / len(self.actions[s]))
                   for s in range(self.size)]
+        for s in range(self.size):
+            self.b[s][self.pi[s]] += 1 - eps
+        # self.b = [np.full(len(self.actions[s]), 1 / len(self.actions[s]))
+        #           for s in range(self.size)]
         c = [[0 for _ in self.actions[s]] for s in range(self.size)]
         num = 0
         for num in range(num_epsd // batch_size):
@@ -276,11 +298,11 @@ class MCAgent(Agent):
             print(f"Episodes {num * batch_size} - {(num + 1) * batch_size} complete in {round(end_time - start_time, 2)}s.")
             num += 1
     
-    def _load_convert(self):
-        self.pi = np.array(self.pi)
+    def load_convert(self):
+        pass
     
-    def _save_convert(self):
-        self.pi = self.pi.tolist()
+    def save_convert(self):
+        pass
 
     def display(self, display_type):
         pass
@@ -288,3 +310,7 @@ class MCAgent(Agent):
     @abstractmethod
     def get_episode(self):
         pass
+
+
+def random_argmax(arr):
+    return int(np.random.choice(np.flatnonzero(arr == np.max(arr, axis=0))))
