dbc7d3bb0052960b3e6ab442b480cd14b727bd74
Magikarp4000 <1fishyblub1@gmail.com>
2025-01-01 17:21:12 +0800

    Fix Dyna

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ class TestEnv(envs.DiscreteEnv):
 
 
 env = TestEnv()
-algo = Dyna(ExploreBonus(TreeLearn(gamma=0.9, nstep=5), kappa=0), plan_algo=QLearn(), nsim=5)
+algo = Dyna(ExploreBonus(TreeLearn(gamma=0.9, nstep=5), kappa=1), plan_algo=QLearn(), nsim=0)
 # algo = TreeLearn(gamma=0.9, nstep=1)
 
 agent = Tabular(env, algo, alpha=0.1)
