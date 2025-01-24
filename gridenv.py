55187dea829e72e1ce42d0e8d9679efbebf96b72
Magikarp4000 <1fishyblub1@gmail.com>
2025-01-24 23:34:30 +0000

    Add mouse drag bounds

--- gridenv.py
+++ gridenv.py
@@ -15,18 +15,25 @@ class GridSquare(QGraphicsRectItem):
 class GridScene(EnvScene):
     def __init__(self, width, height):
         super().__init__(width, height)
+        self.grid_width = CELL_SIZE * NUM_CELLS_X
+        self.grid_height = CELL_SIZE * NUM_CELLS_Y
         self.grid = [[GridSquare(x, y, CELL_SIZE) for x in range(NUM_CELLS_X)]
                      for y in range(NUM_CELLS_Y)]
         self.cells = self.createItemGroup(flatten(self.grid))
-        self.mouse_pos = None
     
     def mouseMoveEvent(self, event):
-        new_pos = event.scenePos()
-        if self.mouse_pos != None and Qt.MouseButton.LeftButton in event.buttons():
-            dx = (new_pos - self.mouse_pos).x()
-            dy = (new_pos - self.mouse_pos).y()
-            self.cells.moveBy(dx, dy)
-        self.mouse_pos = new_pos
+        d_pos = event.scenePos() - event.lastScenePos()
+        if Qt.MouseButton.LeftButton in event.buttons():
+            self.cells.moveBy(*d_pos.toTuple())
+
+            if self.cells.x() < 0:
+                self.cells.setX(0)
+            if self.cells.x() > self.width() - self.grid_width:
+                self.cells.setX(self.width() - self.grid_width)
+            if self.cells.y() < 0:
+                self.cells.setY(0)
+            if self.cells.y() > self.height() - self.grid_height:
+                self.cells.setY(self.height() - self.grid_height)
 
 
 if __name__ == '__main__':
